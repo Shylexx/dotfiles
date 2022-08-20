1d7d07922bf5b083a3dedf911b0161ebc12e49fe
Shylexx <alexanderdauncey@gmail.com>
2022-08-20 22:34:44 +0100

    Fixing Rust Tools

--- .config/nvim/lua/shylex/nvimlsp.lua
+++ .config/nvim/lua/shylex/nvimlsp.lua
@@ -57,41 +57,9 @@ require('lspconfig')['hls'].setup{
 -- nvim_lsp object
 local nvim_lsp = require'lspconfig'
 
-local opts = {
-    tools = {
-        autoSetHints = true,
-        hover_with_actions = true,
-        runnables = {
-            use_telescope = true
-        },
-        inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
+local rt = require("rust-tools")
 
-    -- all the opts to send to nvim-lspconfig
-    -- these override the de
-    -- require('lspconfig')[haskel:faults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = {
-        -- on_attach is a callback called when the language server attachs to the buffer
-        -- on_attach = on_attach,
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
-        }
-    },
-}
-
-require('rust-tools').setup(opts)
-
--- Set inlay hints
-require('rust-tools.inlay_hints').disable_inlay_hints()
+rt.setup({
+  server = {
+  },
+})
